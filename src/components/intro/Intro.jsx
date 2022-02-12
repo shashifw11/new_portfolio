237b2185d28e0402e66b7d225f55a3a6bab8d673
shashifw11 <shashi_fw11_220@masai.school>
2022-02-12 19:37:39 +0530

    add

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -12,7 +12,7 @@ export default function Intro() {
       showCursor: true,
       backDelay: 1500,
       backSpeed:60,
-      strings: ["Frontend Developer", "Full-Stack Web Developer"],
+      strings: ["software Engineer", "Full-Stack Web Developer"],
     });
   }, []);
 
