b775ad0f213fa50999108e7fa6ac6690533f210c
shashifw11 <shashi_fw11_220@masai.school>
2022-11-11 01:55:43 +0530

    add resume

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -28,7 +28,7 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
           
             
             </div>
-               <div className="itemContainer" onClick={() => window.open("assets/Shashi_kumar.pdf", "_blank")}>
+               <div className="itemContainer" onClick={() => window.open("assets\Shashi_Kumar.pdf", "_blank")}>
               <span style={{ display: "flex" }}><GetAppIcon style={{height:"18px",margin:"2px",borderBottom:"none"}}/>  Resume</span>
           
             
